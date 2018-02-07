921d3135b1c6a9a5091ef1a4639726c2d18b6f37
Szilard Ledan <szledan@gmail.com>
2018-02-07 23:06:56 +0100

    Initial commit of dEnum
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/* Copyright (C) 2018, Szilard Ledan <szledan@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+
+#define dEnum(ENUM, ENUM_NAMES, ...) \
+    enum ENUM { \
+      __VA_ARGS__ \
+    }; \
+     \
+    std::map<int, std::string> ENUM_NAMES = [](){ std::map<int, std::string> _m; int _v = 0; std::string _t; std::stringstream _ss(#__VA_ARGS__); \
+        while(std::getline(_ss, _t, ',')) { \
+            const size_t pos = _t.find('='); if (pos != std::string::npos) _v = std::atoi(_t.substr(pos + 1).c_str()); \
+            { std::stringstream _ss_; _ss_ << _t; _ss_ >> _t; } _m[_v] = _t; _v++; \
+        } \
+        return _m; \
+    }();
+
+dEnum( MyEnum, namesInMyEnum,
+    First,
+    Second,
+    Out = 10,
+    In = 6,
+    Last,
+)
+
+int main(int argc, char* argv[])
+{
+    for (int e = MyEnum::First; e <= MyEnum::Last; ++e)
+        std::cout << namesInMyEnum[e] << " = " << e << std::endl;
+
+    std::cout << namesInMyEnum[MyEnum::Out] << " = " << MyEnum::Out << std::endl;
+
+    return 0;
+}
